6f873fb4332d39103718f3e4b20afc4217819e42
la-ki <layarkostic94@gmail.com>
2020-09-03 12:42:02 +0200

    Added Widgets component

--- src/components/Widgets/Widgets.js
+++ src/components/Widgets/Widgets.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './Widgets.css';
 
+import {
+    TwitterTimelineEmbed,
+    TwitterShareButton,
+    TwitterTweetEmbed
+} from 'react-twitter-embed';
+
+import SearchIcon from "@material-ui/icons/Search";
+
 function Widgets() {
     return (
         <div className="widget">
-            
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input type="text" placeholder="Search Twitter" />
+            </div>
+
+            <div className="widgets__widgetContainer">
+                <h2>What's happening</h2>
+
+                <TwitterTweetEmbed tweetId={"1189966818640900096"} />
+
+                <TwitterTimelineEmbed sourceType="profile" screenName="reactjs" options={{height: 400}} />
+
+                <TwitterShareButton 
+                    url={"https://facebook.com/donickoce"}
+                    options={{text: "#reactjs is awesome", via: "reactjs"}}
+                />
+            </div>
         </div>
     )
 }
